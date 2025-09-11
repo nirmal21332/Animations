fd02696b99537485e25ff966b5bd46daea6a9172
nirmal21332 <nirmal93514@gmail.com>
2025-09-11 12:10:43 +0530

    first commit

--- lib/Home_screen.dart
+++ lib/Home_screen.dart
@@ -7,44 +7,60 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late Animation animation;
-  late AnimationController animationController;
-  late Animation colorAnimation;
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+
+  late Animation _animation;
+  late AnimationController _animationController;
+
+  var listRadius=[100.0,150.0,200.0,250.0,300.0];
 
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    animation = Tween(begin: 100.0, end: 200.0).animate(animationController);
-    colorAnimation = ColorTween(
-      begin: Colors.red,
-      end: Colors.green,
-    ).animate(animationController);
-    animationController.addListener(() {
-      setState(() {});
+
+    _animationController=AnimationController(vsync: this,duration: Duration(seconds:4),lowerBound: 0.2);
+    _animation=Tween(begin: 0.0,end: 1.0).animate(_animationController);
+    _animationController.addListener((){
+      setState(() {
+      });
     });
-    animationController.forward();
+    _animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('List Wheel Scroll View'),
+        title: Text('Ripple Effect'),
         centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          color: colorAnimation.value,
-          height: animation.value,
-          width: animation.value,
-        ),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              buildMyContainer(listRadius[0]),
+              buildMyContainer(listRadius[1]),
+              buildMyContainer(listRadius[2]),
+              buildMyContainer(listRadius[3]),
+              buildMyContainer(listRadius[4]),
+Icon(Icons.add_call)
+
+            ]
+          ),
+        )
       ),
     );
+  }
+
+  Container buildMyContainer(double radius) {
+    return Container(
+              width: radius*_animation.value,
+              height: radius*_animation.value,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue.withOpacity(1.0 - _animation.value)
+              ),
+            );
   }
 }
